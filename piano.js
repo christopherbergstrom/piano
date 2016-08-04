f73d82ab3624355cfa0f39e2c4fd510e6b23420c
Christopher Bergstrom <christopherrbergstrom@gmail.com>
2016-08-03 21:04:50 -0700

    working on css

--- piano.js
+++ piano.js
@@ -41,44 +41,6 @@ var gSH;
 
 $(document).ready(function()
 {
-
-  bL = new Audio("keys/bL.wav");
-  cL = new Audio("keys/cL.wav");
-  cSL = new Audio("keys/cSL.wav");
-  dL = new Audio("keys/dL.wav");
-  dSL = new Audio("keys/dSL.wav");
-  eL = new Audio("keys/eL.wav");
-  fL = new Audio("keys/fL.wav");
-  fSL = new Audio("keys/fSL.wav");
-  gL = new Audio("keys/gL.wav");
-  gSL = new Audio("keys/gSL.wav");
-
-  a = new Audio("keys/a.wav");
-  aS = new Audio("keys/aS.wav");
-  b = new Audio("keys/b.wav");
-  c = new Audio("keys/c.wav");
-  cS = new Audio("keys/cS.wav");
-  d = new Audio("keys/d.wav");
-  dS = new Audio("keys/dS.wav");
-  e = new Audio("keys/e.wav");
-  f = new Audio("keys/f.wav");
-  fS = new Audio("keys/fS.wav");
-  g = new Audio("keys/g.wav");
-  gS = new Audio("keys/gS.wav");
-
-  aH = new Audio("keys/aH.wav");
-  aSH = new Audio("keys/aSH.wav");
-  bH = new Audio("keys/bH.wav");
-  cH = new Audio("keys/cH.wav");
-  cSH = new Audio("keys/cSH.wav");
-  dH = new Audio("keys/dH.wav");
-  dSH = new Audio("keys/dSH.wav");
-  eH = new Audio("keys/eH.wav");
-  fH = new Audio("keys/fH.wav");
-  fSH = new Audio("keys/fSH.wav");
-  gH = new Audio("keys/gH.wav");
-  gSH = new Audio("keys/gSH.wav");
-
   $(document).keydown(function(e)
   {
     down[e.which] = true;
@@ -96,95 +58,116 @@ $(document).ready(function()
     }
     else if (down[16] && down[88])
     {
+      bL = new Audio("keys/bL.wav");
       bL.play();
     }
     else if (down[16] && down[67])
     {
+      cL = new Audio("keys/cL.wav");
       cL.play();
     }
     else if (down[16] && down[70])
     {
+      cSL = new Audio("keys/cSL.wav");
       cSL.play();
     }
     else if (down[16] && down[86])
     {
+      dL = new Audio("keys/dL.wav");
       dL.play();
     }
     else if (down[16] && down[71])
     {
+      dSL = new Audio("keys/dSL.wav");
       dSL.play();
     }
     else if (down[16] && down[66])
     {
+      eL = new Audio("keys/eL.wav");
       eL.play();
     }
     else if (down[16] && down[78])
     {
+      fL = new Audio("keys/fL.wav");
       fL.play();
     }
     else if (down[16] && down[74])
     {
+      fSL = new Audio("keys/fSL.wav");
       fSL.play();
     }
     else if (down[16] && down[77])
     {
+      gL = new Audio("keys/gL.wav");
       gL.play();
     }
     else if (down[16] && down[75])
     {
+      gSL = new Audio("keys/gSL.wav");
       gSL.play();
     }
 
     // high
     else if (down[17] && down[90])
     {
+      aH = new Audio("keys/aH.wav");
       aH.play();
     }
     else if (down[17] && down[83])
     {
+      aSH = new Audio("keys/aSH.wav");
       aSH.play();
     }
     else if (down[17] && down[88])
     {
+      bH = new Audio("keys/bH.wav");
       bH.play();
     }
     else if (down[17] && down[67])
     {
+      cH = new Audio("keys/cH.wav");
       cH.play();
     }
     else if (down[17] && down[70])
     {
+      cSH = new Audio("keys/cSH.wav");
       cSH.play();
     }
     else if (down[17] && down[86])
     {
+      dH = new Audio("keys/dH.wav");
       dH.play();
     }
     else if (down[17] && down[71])
     {
+      dSH = new Audio("keys/dSH.wav");
       dSH.play();
     }
     else if (down[17] && down[66])
     {
+      eH = new Audio("keys/eH.wav");
       eH.play();
     }
     else if (down[17] && down[78])
     {
+      fH = new Audio("keys/fH.wav");
       fH.play();
     }
     else if (down[17] && down[74])
     {
+      fSH = new Audio("keys/fSH.wav");
       fSH.play();
     }
     else if (down[17] && down[77])
     {
+      gH = new Audio("keys/gH.wav");
       gH.play();
     }
     else if (down[17] && down[75])
     {
+      gSH = new Audio("keys/gSH.wav");
       gSH.play();
     }
-
 
     // medium
     else if (down[90])
@@ -194,46 +177,57 @@ $(document).ready(function()
     }
     else if (down[83])
     {
+      aS = new Audio("keys/aS.wav");
       aS.play();
     }
     else if (down[88])
     {
+      b = new Audio("keys/b.wav");
       b.play();
     }
     else if (down[67])
     {
+      c = new Audio("keys/c.wav");
       c.play();
     }
     else if (down[70])
     {
+      cS = new Audio("keys/cS.wav");
       cS.play();
     }
     else if (down[86])
     {
+      d = new Audio("keys/d.wav");
       d.play();
     }
     else if (down[71])
     {
+      dS = new Audio("keys/dS.wav");
       dS.play();
     }
     else if (down[66])
     {
+      e = new Audio("keys/e.wav");
       e.play();
     }
     else if (down[78])
     {
+      f = new Audio("keys/f.wav");
       f.play();
     }
     else if (down[74])
     {
+      fS = new Audio("keys/fS.wav");
       fS.play();
     }
     else if (down[77])
     {
+      g = new Audio("keys/g.wav");
       g.play();
     }
     else if (down[75])
     {
+      gS = new Audio("keys/gS.wav");
       gS.play();
     }
   }).keyup(function(e)
