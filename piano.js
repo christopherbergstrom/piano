568f2b788081c5f05232fde0b784331684bdee4b
Christopher Bergstrom <christopherrbergstrom@gmail.com>
2016-08-04 09:50:17 -0700

    working on changing colors on press and release

--- piano.js
+++ piano.js
@@ -1,5 +1,21 @@
 var down = [];
 
+var abtn;
+var aSbtn;
+var bbtn;
+var cbtn;
+var cSbtn;
+var dbtn;
+var dSbtn;
+var ebtn;
+var fbtn;
+var fSbtn;
+var gbtn;
+var gSbtn;
+
+var shift;
+var ctrl;
+
 var aL;
 var aSL;
 var bL;
@@ -41,6 +57,22 @@ var gSH;
 
 $(document).ready(function()
 {
+  abtn = $(".a");
+  aSbtn = $(".aS");
+  bbtn = $(".b");
+  cbtn = $(".b");
+  cSbtn = $(".bS");
+  dbtn = $(".d");
+  dSbtn = $(".dS");
+  ebtn = $(".e");
+  fbtn = $(".f");
+  fSbtn = $(".fS");
+  gbtn = $(".g");
+  gSbtn = $(".gS");
+
+  shift = $("#shift");
+  ctrl = $("#ctrl");
+
   $(document).keydown(function(e)
   {
     down[e.which] = true;
@@ -51,187 +83,255 @@ $(document).ready(function()
       aL = new Audio("keys/aL.wav");
       aL.play();
     }
-    else if (down[16] && down[83])
+    if (down[16] && down[83])
     {
       aSL = new Audio("keys/aSL.wav");
       aSL.play();
     }
-    else if (down[16] && down[88])
+    if (down[16] && down[88])
     {
       bL = new Audio("keys/bL.wav");
       bL.play();
     }
-    else if (down[16] && down[67])
+    if (down[16] && down[67])
     {
       cL = new Audio("keys/cL.wav");
       cL.play();
     }
-    else if (down[16] && down[70])
+    if (down[16] && down[70])
     {
       cSL = new Audio("keys/cSL.wav");
       cSL.play();
     }
-    else if (down[16] && down[86])
+    if (down[16] && down[86])
     {
       dL = new Audio("keys/dL.wav");
       dL.play();
     }
-    else if (down[16] && down[71])
+    if (down[16] && down[71])
     {
       dSL = new Audio("keys/dSL.wav");
       dSL.play();
     }
-    else if (down[16] && down[66])
+    if (down[16] && down[66])
     {
       eL = new Audio("keys/eL.wav");
       eL.play();
     }
-    else if (down[16] && down[78])
+    if (down[16] && down[78])
     {
       fL = new Audio("keys/fL.wav");
       fL.play();
     }
-    else if (down[16] && down[74])
+    if (down[16] && down[74])
     {
       fSL = new Audio("keys/fSL.wav");
       fSL.play();
     }
-    else if (down[16] && down[77])
+    if (down[16] && down[77])
     {
       gL = new Audio("keys/gL.wav");
       gL.play();
     }
-    else if (down[16] && down[75])
+    if (down[16] && down[75])
     {
       gSL = new Audio("keys/gSL.wav");
       gSL.play();
     }
 
     // high
-    else if (down[17] && down[90])
+    if (down[17] && down[90])
     {
       aH = new Audio("keys/aH.wav");
       aH.play();
     }
-    else if (down[17] && down[83])
+    if (down[17] && down[83])
     {
       aSH = new Audio("keys/aSH.wav");
       aSH.play();
     }
-    else if (down[17] && down[88])
+    if (down[17] && down[88])
     {
       bH = new Audio("keys/bH.wav");
       bH.play();
     }
-    else if (down[17] && down[67])
+    if (down[17] && down[67])
     {
       cH = new Audio("keys/cH.wav");
       cH.play();
     }
-    else if (down[17] && down[70])
+    if (down[17] && down[70])
     {
       cSH = new Audio("keys/cSH.wav");
       cSH.play();
     }
-    else if (down[17] && down[86])
+    if (down[17] && down[86])
     {
       dH = new Audio("keys/dH.wav");
       dH.play();
     }
-    else if (down[17] && down[71])
+    if (down[17] && down[71])
     {
       dSH = new Audio("keys/dSH.wav");
       dSH.play();
     }
-    else if (down[17] && down[66])
+    if (down[17] && down[66])
     {
       eH = new Audio("keys/eH.wav");
       eH.play();
     }
-    else if (down[17] && down[78])
+    if (down[17] && down[78])
     {
       fH = new Audio("keys/fH.wav");
       fH.play();
     }
-    else if (down[17] && down[74])
+    if (down[17] && down[74])
     {
       fSH = new Audio("keys/fSH.wav");
       fSH.play();
     }
-    else if (down[17] && down[77])
+    if (down[17] && down[77])
     {
       gH = new Audio("keys/gH.wav");
       gH.play();
     }
-    else if (down[17] && down[75])
+    if (down[17] && down[75])
     {
       gSH = new Audio("keys/gSH.wav");
       gSH.play();
     }
 
     // medium
-    else if (down[90])
+    if (down[90])
     {
+      abtn.addClass("medium");
       a = new Audio("keys/a.wav");
       a.play();
     }
-    else if (down[83])
+    if (down[83])
     {
+      aSbtn.addClass("medium");
       aS = new Audio("keys/aS.wav");
       aS.play();
     }
-    else if (down[88])
+    if (down[88])
     {
+      bbtn.addClass("medium");
       b = new Audio("keys/b.wav");
       b.play();
     }
-    else if (down[67])
+    if (down[67])
     {
+      cbtn.addClass("medium");
       c = new Audio("keys/c.wav");
       c.play();
     }
-    else if (down[70])
+    if (down[70])
     {
+      cSbtn.addClass("medium");
       cS = new Audio("keys/cS.wav");
       cS.play();
     }
-    else if (down[86])
+    if (down[86])
     {
+      dbtn.addClass("medium");
       d = new Audio("keys/d.wav");
       d.play();
     }
-    else if (down[71])
+    if (down[71])
     {
+      dSbtn.addClass("medium");
       dS = new Audio("keys/dS.wav");
       dS.play();
     }
-    else if (down[66])
+    if (down[66])
     {
+      ebtn.addClass("medium");
       e = new Audio("keys/e.wav");
       e.play();
     }
-    else if (down[78])
+    if (down[78])
     {
+      fbtn.addClass("medium");
       f = new Audio("keys/f.wav");
       f.play();
     }
-    else if (down[74])
+    if (down[74])
     {
+      fSbtn.addClass("medium");
       fS = new Audio("keys/fS.wav");
       fS.play();
     }
-    else if (down[77])
+    if (down[77])
     {
+      gbtn.addClass("medium");
       g = new Audio("keys/g.wav");
       g.play();
     }
-    else if (down[75])
+    if (down[75])
     {
+      gSbtn.addClass("medium");
       gS = new Audio("keys/gS.wav");
       gS.play();
     }
   }).keyup(function(e)
   {
     down[e.which] = false;
+    if (!down[16])
+    {
+      shift.removeClass("low medium high");
+    }
+    if (!down[17])
+    {
+      ctrl.removeClass("low medium high");
+    }
+    if (!down[90])
+    {
+      abtn.removeClass("low medium high");
+    }
+    if (!down[83])
+    {
+      aSbtn.removeClass("low medium high");
+    }
+    if (!down[88])
+    {
+      bbtn.removeClass("low medium high");
+    }
+    if (!down[67])
+    {
+      cbtn.removeClass("low medium high");
+    }
+    if (!down[70])
+    {
+      cSbtn.removeClass("low medium high");
+    }
+    if (!down[86])
+    {
+      dbtn.removeClass("low medium high");
+    }
+    if (!down[71])
+    {
+      dSbtn.removeClass("low medium high");
+    }
+    if (!down[66])
+    {
+      ebtn.removeClass("low medium high");
+    }
+    if (!down[78])
+    {
+      fbtn.removeClass("low medium high");
+    }
+    if (!down[74])
+    {
+      fSbtn.removeClass("low medium high");
+    }
+    if (!down[77])
+    {
+      gbtn.removeClass("low medium high");
+    }
+    if (!down[75])
+    {
+      gSbtn.removeClass("low medium high");
+    }
   });
 });
